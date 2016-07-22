4f56f5fa04232e0b1e2158cf50dfa58fe0988cef
Thomas Bytheway <tb403@cam.ac.uk>
2016-07-22 17:34:49 +0100

    First pass working graph diff, view in libs/prov-graph/test.html

--- lib/prov-graphs/diff.py
+++ lib/prov-graphs/diff.py
@@ -1,158 +1,164 @@
-import collections
+import json
+import sys
 
-mat_elm = collections.namedtuple("mat_elm", ["pos", "val", "base_n", "new_n"])
+
+def graph(g):
+    nodes = {n['id']: {'id': n['id'],
+                       'name': n['name'],
+                       'type': n['type'],
+                       'parents': []}
+             for n in g['nodes']}
+    root = nodes[g['root']]
+    for edge in g['edges']:
+        if edge['type'] == 'rw':
+            nodes[edge['src']]['parents'].append(nodes[edge['dest']])
+        nodes[edge['dest']]['parents'].append(nodes[edge['src']])
+    return nodes, root
 
 
-class Node():
-    def __init__(self, nid, n_type, name, out_deg, in_deg):
-        self.nid = nid
-        self.n_type = n_type
-        self.name = name
-        self.out_deg = out_deg
-        self.in_deg = in_deg
+def diff(ga, gb):
+    ga_n, ga_r = graph(ga)
+    gb_n, gb_r = graph(gb)
 
-    def _get_diff(self, b):
-        ret = []
-        if self.n_type == b.n_type:
-            ret += [0.0]
+    o_n_all, o_n_a, o_n_b, o_r = [], {}, {}, {'parents': [], 'chg': 'none'}
+
+    o_n_a[ga_r['id']] = o_n_b[gb_r['id']] = o_r
+
+    queue = [(ga_r, gb_r, o_r)]
+
+    while len(queue):
+        cur_a, cur_b, cur_o = queue.pop()
+
+        o_n_all.append(cur_o)
+
+        if cur_a is not None:
+            cur_o['name'] = cur_a['name']
+            cur_o['type'] = cur_a['type']
+            cur_o['lid'] = cur_a['id']
+
+            par_a = list(cur_a['parents'])
         else:
-            ret += [1.0]
+            par_a = []
 
-        if self.name == b.name:
-            ret += [0.0]
+        if cur_b is not None:
+            cur_o['name'] = cur_b['name']
+            cur_o['type'] = cur_b['type']
+            cur_o['rid'] = cur_b['id']
+
+            par_b = list(cur_b['parents'])
         else:
-            ret += [1.0]
+            par_b = []
 
-        in_diff = abs(self.in_deg-b.in_deg)
-        out_diff = abs(self.out_deg-b.out_deg)
-        ret += [in_diff/(1.0+in_diff)]
-        ret += [out_diff/(1.0+out_diff)]
+        while len(par_a):
+            p_a = par_a.pop()
+            p_b_l = [p
+                     for p in par_b
+                     if p['type'] == p_a['type'] and
+                     p['name'] == p_a['name']]
 
-        return ret
+            if p_b_l:
+                p_b = p_b_l[0]
+                par_b.remove(p_b)
 
-    def diff(self, b):
-        diffs = self._get_diff(b)
-        val = sum(diffs)/len(diffs)
-        print("Diff: {} vs {} = {}".format(self.nid, b.nid, val))
-        return val
+                if p_a['id'] in o_n_a:
+                    p_o = o_n_a[p_a['id']]
 
-    def render(self):
-        return {'id': self.nid,
-                'type': self.n_type,
-                'name': self.name}
+                    if 'rid' not in p_o:
+                        p_o['rid'] = p_b['id']
+                        p_o['chg'] = 'none'
+                        o_n_b[p_b['id']] = p_o
+                elif p_b['id'] in o_n_b:
+                    p_o = o_n_b[p_b['id']]
 
+                    if 'lid' not in p_o:
+                        p_o['lid'] = p_a['id']
+                        p_o['chg'] = 'none'
+                        o_n_a[p_a['id']] = p_o
+                else:
+                    p_o = {'parents': [], 'chg': 'none'}
+                    o_n_a[p_a['id']] = p_o
+                    o_n_b[p_b['id']] = p_o
+                    queue.append((p_a, p_b, p_o))
 
-def build_mat(base, new):
-    return [[mat_elm((x, y),
-                     node_b.diff(node_n),
-                     node_b,
-                     node_n)
-             for y, node_n in enumerate(new)]
-            for x, node_b in enumerate(base)]
-
-
-def matrixmin(mat):
-    return min(min(mat,
-                   key=lambda col: min(col,
-                                       key=lambda elm: elm.val).val),
-               key=lambda elm: elm.val)
-
-
-def matrixnull(mat, elm):
-    ''' Null a single element of the matrix.'''
-    mat[elm.pos[0]][elm.pos[1]] = mat_elm(elm.pos, 2**32, None, None)
-
-
-def matrixremove(mat, elm):
-    ''' Kill the row and column and element is on. '''
-    for col in mat:
-        tmp = col[elm.pos[1]]
-        col[elm.pos[1]] = mat_elm(tmp.pos, 2**32, None, None)
-    for i in range(len(mat[elm.pos[0]])):
-        tmp = mat[elm.pos[0]][i]
-        mat[elm.pos[0]][i] = mat_elm(tmp.pos, 2**32, None, None)
-
-
-def non_aligned(nodes):
-    x = set()
-    y = set()
-    for elm in nodes:
-        x.add(elm.pos[0])
-        y.add(elm.pos[1])
-    return len(x) == len(nodes) == len(y)
-
-
-def get_matching(base_d, new_d, base_render=True):
-    out = []
-
-    mat = build_mat(base_d, new_d)
-
-    while True:
-        cur = [matrixmin(mat)]
-
-        if cur[0].val == 2**32:
-            break
-        matrixnull(mat, cur[0])
-
-        while True:
-            next_min = matrixmin(mat)
-            if next_min.val == cur[0].val:
-                cur += [next_min]
-                matrixnull(mat, next_min)
+                cur_o['parents'].append(p_o)
             else:
-                print("min group found")
-                break
-
-        if non_aligned(cur):
-            for elm in cur:
-                if base_render:
-                    o_node = elm.base_n.render()
+                if p_a['id'] in o_n_a:
+                    p_o = o_n_a[p_a['id']]
                 else:
-                    o_node = elm.new_n.render()
-                o_node['bid'] = elm.base_n.nid
-                o_node['nid'] = elm.new_n.nid
-                if(elm.base_n.n_type == elm.new_n.n_type and
-                   elm.base_n.name == elm.new_n.name):
-                    o_node['chg'] = 'none'
-                else:
-                    o_node['chg'] = 'mod'
-                out.append(o_node)
+                    p_o = {'parents': [], 'chg': 'del'}
+                    o_n_a[p_a['id']] = p_o
+                    queue.append((p_a, None, p_o))
+                cur_o['parents'].append(p_o)
 
-                matrixremove(mat, elm)
-        else:
-            print("Error: Ambiguous match, aborting for now.")
-            return
+        for p_b in par_b:
+            if p_b['id'] in o_n_b:
+                p_o = o_n_b[p_b['id']]
+            else:
+                p_o = {'parents': [], 'chg': 'add'}
+                o_n_b[p_b['id']] = p_o
+                queue.append((None, p_b, p_o))
+            cur_o['parents'].append(p_o)
 
-    base_assigned = set([n['bid'] for n in out])
+    out_nodes = []
+    out_nodes_l = {}
+    out_nodes_r = {}
 
-    base_remain = [elm for elm in base_d if elm.nid not in base_assigned]
+    for node in o_n_all:
+        out_node = {'type': node['type'],
+                    'name': node['name'],
+                    'chg': node['chg']}
 
-    for elm in base_remain:
-        o_node = elm.render()
-        o_node['chg'] = 'del'
-        o_node['bid'] = elm.nid
-        o_node['nid'] = None
-        out.append(o_node)
+        nid = " - "
+        if 'lid' in node:
+            nid = str(node['lid']) + nid
+            out_nodes_l[node['lid']] = out_node
 
-    new_assigned = set([n['nid'] for n in out])
+        if 'rid' in node:
+            nid = nid + str(node['rid'])
+            out_nodes_r[node['rid']] = out_node
+        out_node['id'] = nid
+        out_nodes.append(out_node)
 
-    new_remain = [elm for elm in new_d if elm.nid not in new_assigned]
+    out_e = []
+    for edge in ga['edges']:
+        src_n = out_nodes_l[edge['src']]
+        dest_n = out_nodes_l[edge['dest']]
+        out_e.append((src_n, dest_n, edge['type'], 'del'))
 
-    for elm in new_remain:
-        o_node = elm.render()
-        o_node['chg'] = 'add'
-        o_node['bid'] = None
-        o_node['nid'] = elm.nid
-        out.append(o_node)
+    for edge in gb['edges']:
+        src_n = out_nodes_r[edge['src']]
+        dest_n = out_nodes_r[edge['dest']]
 
-    return out
+        new_out = []
+        found = False
+        for s, d, t, c in out_e:
+            if s == src_n and d == dest_n:
+                new_out.append((s, d, t, 'none'))
+                found = True
+            else:
+                new_out.append((s, d, t, c))
+        if not found:
+            new_out.append((src_n, dest_n, edge['type'], 'add'))
+        out_e = new_out
 
-m = get_matching([Node('a', 'proc', '/bin/bash', 1, 2),
-                  Node('b', 'file', 'foo.txt', 1, 1),
-                  Node('c', 'file', 'bar.txt', 1, 1),
-                  Node('d', 'file', 'baz.txt', 1, 0),
-                  Node('e', 'proc', '/bin/vim', 0, 2)],
-                 [Node(1, 'proc', '/bin/bash', 1, 1),
-                  Node(2, 'file', 'foo.txt', 1, 1),
-                  Node(3, 'file', 'bar.txt', 1, 0),
-                  Node(4, 'proc', '/bin/vim', 0, 1)])
+    out_edges = [{'src': s['id'],
+                  'dest': d['id'],
+                  'type': t,
+                  'chg': c}
+                 for s, d, t, c in out_e]
+
+    return {"root": out_nodes_l[ga['root']]['id'],
+            "nodes": out_nodes,
+            "edges": out_edges}
+
+with open(sys.argv[1]) as g1_f:
+    g1 = json.load(g1_f)
+
+with open(sys.argv[2]) as g2_f:
+    g2 = json.load(g2_f)
+
+o = diff(g1, g2)
+
+with open(sys.argv[3], "w") as o_f:
+    json.dump(o, o_f)
+# m = get_matching(nodify(g1), nodify(g2))
