87f782536dbb935e444c4b773a7482eff6fb7d7f
Nikilesh Balakrishnan <nb466@cl.cam.ac.uk>
2016-08-05 13:52:37 +0100

    fixed proc hierarcy to include processes that do not have parents or children

--- query/proc_hierarchy.py
+++ query/proc_hierarchy.py
@@ -12,12 +12,27 @@ import json
 
 proc_map = {}
 
+user_map = {}
+user_map['8314'] = {'name': 'bob', 'IP': '192.168.1.1'}
+user_map['8315'] = {'name': 'alice', 'IP': '192.168.1.20'}
+user_map['8316'] = {'name': 'eve', 'IP': '192.168.1.21'}
+user_map['0'] = {'name': 'root', 'IP': ''}
+
 #db = GraphDatabase("/local/scratch/nik/opus_stuff/prov.neo4j")
 db = GraphDatabase("/auto/homes/nb466/Downloads/prov.neo4j")
 
 def get_date_time_str(sys_time):
     return datetime.datetime.fromtimestamp(sys_time).strftime(
         '%Y-%m-%d %H:%M:%S')
+
+def get_user_rec(proc):
+    if proc.OTHER_META.outgoing:
+        for tmp_rel in proc.OTHER_META.outgoing:
+            meta = tmp_rel.end
+            if meta.has_key("name") and meta['name'] == 'uid':
+                user = meta['value']
+                return user_map[user]
+
 
 def get_proc_data(pnode_id):
     rows = db.query("START p=node({id}) "
@@ -31,9 +46,16 @@ def get_proc_data(pnode_id):
     gnode_id = 0
     process_id = 0
     pstatus = ""
+    user_name = ''
+    user_ip = ''
     for row in rows:
         glob = row['g']
         proc = row['p']
+        user_rec = get_user_rec(proc)
+        if user_rec:
+            user_name = user_rec['name']
+            user_ip = user_rec['IP']
+
         exe_name = glob['name'][0]
         sys_time = get_date_time_str(proc['sys_time'])
         gnode_id = glob.id
@@ -42,11 +64,11 @@ def get_proc_data(pnode_id):
             pstatus = "DEAD"
         else:
             pstatus = "ALIVE"
-    return exe_name, sys_time, gnode_id, process_id, pstatus
+    return exe_name, sys_time, gnode_id, process_id, user_name, user_ip
 
 
 def get_all_procs():
-    print("Getting all processes in the graph")
+    #print("Getting all processes in the graph")
     all_procs = []
 
     rows = db.query("START n=node(*) "
@@ -56,13 +78,14 @@ def get_all_procs():
         p = row['n']
         all_procs.append(p.id)
 
-    print("Total number of processes: %d" % (len(all_procs)))
+    #print("Total number of processes: %d" % (len(all_procs)))
     return all_procs
 
 
 def get_all_pairs(all_procs):
-    print("Getting all processes pairs in the graph")
+    #print("Getting all processes pairs in the graph")
     pair_list = []
+    solo_procs = []
 
     for p in all_procs:
         pnode = db.node[p]
@@ -70,38 +93,49 @@ def get_all_pairs(all_procs):
             for tmp_rel in pnode.PROC_PARENT.outgoing:
                 parent = tmp_rel.end
                 pair_list.append([parent.id, pnode.id, "forked"])
-
-        if pnode.PROC_OBJ_PREV.outgoing:
+        elif pnode.PROC_OBJ_PREV.outgoing:
             for tmp_rel in pnode.PROC_OBJ_PREV.outgoing:
                 prevnode = tmp_rel.end
                 pair_list.append([prevnode.id, pnode.id, "execed"])
+        else:
+            solo_procs.append(p)
 
-    return pair_list
+    return pair_list, solo_procs
 
 
 def get_proc_obj(p):
     proc_node = db.node[p]
 
-    print("Getting process data for PID: %d" % proc_node['pid'])
-    exe, sys_time, gnode_id, pid, pstatus = get_proc_data(p)
+    #print("Getting process data for PID: %d" % proc_node['pid'])
+    exe, sys_time, gnode_id, pid, user_name, user_ip = get_proc_data(p)
 
     if p in proc_map:
         return proc_map[p]
     else:
-        return {'id': p, 'gnode_id': gnode_id, 'exe': exe, 'pid': pid, 'time': '', 'children': []}
-        #return {'id': p, 'exe': exe, 'time': sys_time, 'children': []}
-    return pobj
+        rec = {'id': p, 'gnode_id': gnode_id,
+                'exe': exe, 'pid': pid,
+                'time': '', 'children': []}
+        if len(user_name) > 0:
+            rec['user'] = user_name
+        if len(user_ip) > 0:
+            rec['ip'] = user_ip
+        return rec
+#    return pobj
 
 
 def print_map(indent, pobj):
     #print("\t"*indent + " " + str(pobj['id']) + " " + pobj['exe'] + " - " + pobj['time'] + " Glob: " + str(pobj['gnode_id']))
-    print("\t"*indent + "%d %s %d - %d" % (pobj['id'], pobj['exe'], pobj['gnode_id'], pobj['pid']))
+    #print("\t"*indent + "%d %s %d - %d" % (pobj['id'], pobj['exe'], pobj['gnode_id'], pobj['pid']))
 
     for p in pobj['children']:
         print_map(indent + 1, p)
 
 
-def treefy(all_procs, pair_list):
+def treefy(pair_list, solo_procs):
+    for p in solo_procs:
+        pobj = get_proc_obj(p)
+        proc_map[p] = pobj
+
     for rel in pair_list:
         p1 = rel[0]
         p2 = rel[1]
@@ -122,28 +156,28 @@ def treefy(all_procs, pair_list):
         proc_map[p1] = pobj1
 
     for k, v in dict.items(proc_map):
-        print("------------")
+        #print("------------")
         #print(str(v['id']) + " " + v['exe'] + " - " + v['time'] + " Glob: " + str(v['gnode_id']))
-        print("%d %s %d - %d" % (v['id'], v['exe'], v['gnode_id'], v['pid']))
+        #print("%d %s %d - %d" % (v['id'], v['exe'], v['gnode_id'], v['pid']))
         for p in v['children']:
             print_map(1, p)
 
 
 all_procs = get_all_procs()
 #print(all_procs)
-print("------------")
+#print("------------")
 
-pair_list = get_all_pairs(all_procs)
+pair_list, solo_procs = get_all_pairs(all_procs)
 #print(pair_list)
 
-treefy(all_procs, pair_list)
+treefy(pair_list, solo_procs)
 
 json_output = []
 for k, v in dict.items(proc_map):
     json_output.append(v)
 
-#print(json.dumps(json_output, indent=4))
-with open('process_tree.json', 'w') as fp:
-    json.dump(json_output, fp)
+print(json.dumps(json_output, indent=4))
+#with open('process_tree.json', 'w') as fp:
+#    json.dump(json_output, fp)
 
 db.shutdown()
