cf1ae287fecbb33944e0294fb223ea66e20fce69
Jonathan Anderson <jonathan.anderson@ieee.org>
2017-06-20 00:11:37 -0230

    Add a playground for Cytoscape exploration.
    This doesn't incorporate previously-recorded traces, nor does it
    integrate with the JS-based application framework of the previous OPUS
    demo, but it does provide a place to explore how we can use Cytoscape
    to implement the "worksheet model" that we seem to be moving towards
    in issue #3.

---
+++ playground/mock_data.py
@@ -0,0 +1,98 @@
+import uuid
+
+def node(ty):
+    return {
+        'id': uuid.uuid4(),
+        'type': ty,
+        'in': set(),
+        'out': set(),
+    }
+
+def process(execname):
+    n = node('process')
+    n.update({
+        'label': execname,
+        'execname': execname,
+    })
+    return n
+
+def vnode():
+    n = node('vnode')
+    u = n['id']
+    n.update({
+        'label': u.hex[:7],
+    })
+    return n
+
+def run(parent, child):
+    u = uuid.uuid4()
+
+    parent['out'].add(u)
+    child['in'].add(u)
+
+    return {
+        'id': u,
+        'label': '',
+        'type': 'exec',
+        'source': parent['id'],
+        'target': child['id'],
+    }
+
+def syscall(name, source, dest):
+    u = uuid.uuid4()
+
+    source['out'].add(u)
+    dest['in'].add(u)
+
+    return {
+        'id': u,
+        'label': name,
+        'type': 'syscall',
+        'source': source['id'],
+        'target': dest['id'],
+    }
+
+
+chrome = process('chrome')
+driveby = process('sh')
+fetch_attack = process('fetch')
+attack_sh = vnode()
+run_attack = process('sh')
+fetch_exfil = process('fetch')
+exfil_sh = vnode()
+run_exfil = process('sh')
+git_recon = process('git')
+git_fetch = process('git')
+
+
+all_nodes = (
+    attack_sh,
+    exfil_sh,
+
+    chrome,
+    driveby,
+    fetch_attack,
+    run_attack,
+    fetch_exfil,
+    run_exfil,
+    git_recon,
+    git_fetch,
+)
+
+nodes = dict([ (n['id'], n) for n in all_nodes ])
+
+all_events = [
+    run(chrome, driveby),
+    run(driveby, fetch_attack),
+    syscall('write', fetch_attack, attack_sh),
+    run(driveby, run_attack),
+    syscall('mmap', attack_sh, run_attack),
+    run(run_attack, fetch_exfil),
+    syscall('write', fetch_exfil, exfil_sh),
+    run(run_attack, run_exfil),
+    syscall('mmap', exfil_sh, run_exfil),
+    run(run_exfil, git_recon),
+    run(run_exfil, git_fetch),
+]
+
+events = dict([ (e['id'], e) for e in all_events ])
---
+++ playground/webapp.py
@@ -0,0 +1,132 @@
+# Copyright 2017 Jonathan Anderson
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#     http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+
+import flask
+import flask_bootstrap
+import flask_dotenv
+import json
+import nav
+import uuid
+
+frontend = flask.Blueprint('frontend', __name__)
+
+
+@frontend.route('/')
+def index():
+    return flask.render_template('index.html')
+
+
+def node(n):
+    return {
+        'data': {
+            'id': '%s' % n['id'],
+            'label': n['label'],
+        },
+        'classes': n['type'],
+    }
+
+def edge(e):
+    return {
+        'data': {
+            'id': '%s' % e['id'],
+            'label': e['label'],
+            'source': '%s' % e['source'],
+            'target': '%s' % e['target'],
+        },
+        'classes': e['type'],
+    }
+
+@frontend.route('/everything')
+def get_all():
+    if flask.current_app.debug:
+        import mock_data
+
+        nodes = mock_data.all_nodes
+        events = mock_data.all_events
+
+    else:
+        raise ValueError, 'not hooked up to real data yet'
+
+    return json.dumps(
+        [ node(n) for n in nodes ]
+        + [ edge(e) for e in events ]
+    )
+
+@frontend.route('/onehop/<string:name>')
+def get_element(name):
+    if flask.current_app.debug:
+        import mock_data
+
+        node_map = mock_data.nodes
+        event_map = mock_data.events
+
+    else:
+        raise ValueError, 'not hooked up to real data yet'
+
+    u = uuid.UUID(name)
+
+    nodes = []
+    events = []
+
+    if u in node_map.keys():
+        n = node_map[u]
+
+        nodes = [ n ]
+
+        for e in n['in']:
+            ev = event_map[e]
+            nodes.append(node_map[ev['source']])
+            events.append(ev)
+
+        for e in n['out']:
+            ev = event_map[e]
+            nodes.append(node_map[ev['target']])
+            events.append(ev)
+
+    else:
+        raise ValueError, '%s not in %s' % (u, node_map.keys())
+
+    return json.dumps(
+        [ node(n) for n in nodes ]
+        + [ edge(e) for e in events ]
+    )
+
+
+nav.nav.register_element('frontend_top',
+    nav.Navbar(
+        nav.View('OPUS', '.index'),
+    )
+)
+
+
+def create_app():
+    # See http://flask.pocoo.org/docs/patterns/appfactories
+    app = flask.Flask(__name__)
+
+    nav.nav.init_app(app)
+    flask_dotenv.DotEnv().init_app(app, verbose_mode = True)
+
+    flask_bootstrap.Bootstrap(app)
+    app.register_blueprint(frontend)
+
+    app.config['BOOTSTRAP_SERVE_LOCAL'] = True
+
+    if app.debug:
+        app.config['TEMPLATES_AUTO_RELOAD'] = True
+        app.jinja_env.auto_reload = True
+
+        from flaskext.sass import sass
+        sass(app, input_dir = 'assets/sass', output_dir = 'static/style')
+
+    return app
