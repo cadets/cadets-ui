ba6ddb9c844e374e42b03fcb3ecf4f4ce506b398
Jonathan Anderson <jonathan.anderson@ieee.org>
2017-06-23 00:14:13 -0230

    Refactor some graphing JavaScript.
    This will hopefully make the code clearer and also make it easier to re-use parts, e.g., context menus on nodes.

---
+++ playground/static/js/graphing.js
@@ -0,0 +1,98 @@
+//
+// Load a Cytograph CSS file and apply it to a graph.
+//
+// Unfortunately this is hard to do statically.
+//
+function load_graph_style(graphs) {
+  $.ajax('/static/style/cytoscape.css', {
+    dataType: 'text',
+    mimeType: 'text/css',
+    success: function(result) {
+      for (let g of graphs) {
+        g.style(result);
+      }
+    }
+  });
+}
+
+//
+// Apply a named graph layout algorithm to a graph.
+//
+function layout_graph(graph, algorithm) {
+  graph.layout({
+    name: algorithm,
+    rankDir: 'LR',
+    animate: false,
+  })
+  .run();
+};
+
+//
+// Create a context menu and attach it to all nodes in a graph.
+//
+// To position everything properly, this requires that the graph be contained
+// within a DOM element with non-static positioning (i.e., relative, fixed or
+// absolute positioning).
+//
+function attach_context_menu(graph, selector, items) {
+  $.contextMenu({
+    selector: selector,
+    trigger: 'none',
+    items: items,
+    callback: function(action, options) {
+      console.log('central callback');
+      let node = graph.contextMenuNode.data();
+
+      for (let key in items) {
+        if (action == key) {
+          items[key].action(node);
+        }
+      }
+    }
+  });
+
+  graph.$('node').on('tap', function(ev) {
+    // The node currently showing a context menu. These menus are madal and
+    // therefore provide implicit synchronization.
+    graph.contextMenuNode = this;
+
+    var pos = ev.renderedPosition;
+    let offset = $(selector).offset();
+    pos.x += offset.left;
+    pos.y += offset.top;
+
+    $(selector).contextMenu(pos);
+  });
+}
+
+//
+// Extract graphical metadata about a graph node (icon and label) that isn't
+// really appropriate to serve from OPUS/Neo4j.
+//
+function node_metadata(node) {
+  switch (node.type) {
+    case 'machine':
+      return {
+        icon: 'desktop',
+        label: node.names.join(' / '),
+      };
+
+    case 'process':
+      return {
+        icon: 'terminal',
+        label: node.cmdline,
+      };
+
+    case 'file-version':
+      return {
+        icon: 'file-o',
+        label: node.names.join(' / '),
+      };
+
+  default:
+    return {
+      icon: 'question',
+      label: 'unknown',
+    };
+  }
+}
