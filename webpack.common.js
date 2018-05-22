3e76dc192291f62ad4b6ec553c4a846bf4bb8916
Jonathan Anderson <jonathan.anderson@ieee.org>
2018-05-22 15:20:50 -0230

    Move top-level code around, add missing license.

--- webpack.common.js
+++ webpack.common.js
@@ -4,7 +4,7 @@ const HtmlWebpackPlugin = require('html-webpack-plugin');
 
 module.exports = {
 	entry: {
-		app: './src/index.js'
+		app: './src/index.coffee'
 	},
 	plugins: [
 		new CleanWebpackPlugin(['dist']),
