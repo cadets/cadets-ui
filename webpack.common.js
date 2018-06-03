bfd56360fe4df072634602058e931783afb0b3af
Jonathan Anderson <jonathan.anderson@ieee.org>
2018-06-03 18:15:30 -0230

    Restore dagre and cose-bilkent icons.
    Bring font-awesome back.

--- webpack.common.js
+++ webpack.common.js
@@ -23,11 +23,8 @@ module.exports = {
 				loader: 'coffee-loader',
 			},
 			{
-				test: /\.css$/,
-				use: [
-					'style-loader',
-					'css-loader'
-				]
+				test: /\.(s*)css$/,
+				use: ['style-loader', 'css-loader', 'sass-loader']
 			},
 			{
 				test: /\.marko$/,
@@ -53,11 +50,13 @@ module.exports = {
 				}]
 			},
 			{
-				test: /\.(woff|woff2|eot|ttf|otf)$/,
-				use: [
-					'file-loader'
-				]
-			}
+				test: /\.woff(2)?(\?v=[0-9]\.[0-9]\.[0-9])?$/,
+				loader: "url-loader?limit=10000&mimetype=application/font-woff"
+			},
+			{
+				test: /\.(ttf|eot|svg)(\?v=[0-9]\.[0-9]\.[0-9])?$/,
+				loader: "file-loader"
+			},
 		]
 	}
 };
